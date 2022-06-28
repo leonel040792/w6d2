cf6d02569e792ff3f753e825012f655ea6a97ce8
leonelcolina <tang.leonel@gmail.com>
2022-06-28 16:49:24 -0400

    finish 00

--- skeleton/lib/00_attr_accessor_object.rb
+++ skeleton/lib/00_attr_accessor_object.rb
@@ -1,5 +1,16 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
-    # ...
+
+
+    names.each do |name|
+      define_method (name) {instance_variable_get("@#{name}")}
+
+      define_method ("#{name}=") do |k|
+        instance_variable_set("@#{name}",k)
+      end
+      
+
+
+    end
   end
 end
